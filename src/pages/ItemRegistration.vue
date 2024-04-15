ffe6dccdb149aa72da34b01c7dcdc70b4016a1c8
HackSungBoo <hsb@khu.ac.kr>
2024-04-15 01:10:04 +0000

    test

--- src/pages/ItemRegistration.vue
+++ src/pages/ItemRegistration.vue
@@ -105,7 +105,7 @@ export default {
             axios.post("/api/items/", formData).then((response) => {
                 console.log("상품이 성공적으로 등록되었습니다:", response.data);
                 window.alert('상품이 성공적으로 등록되었습니다.');
-                router.push({ path: '/' });
+                router.push({ path: '/loading' });
 
             }).catch((error) => {
                 console.error("상품 등록에 실패했습니다:", error);
--- src/pages/Loading.vue
+++ src/pages/Loading.vue
@@ -5,6 +5,7 @@
     <div class="progress-container">
       <div class="progress-bar" :style="{ width: progressBarWidth }">{{ progressBarWidth }}</div>
     </div>
+    <div>{{ progressInfo }}</div>
   </div>
 </template>
 
@@ -16,25 +17,49 @@ export default {
   data() {
     return {
       progressBarWidth: '0%',
+      progressInfo: '',
       socket: null
     };
   },
   mounted() {
     // WebSocket 연결
     this.socket = new WebSocket("ws://43.200.158.21/ws");
+
     // 연결이 성공하면
     this.socket.onopen = () => {
       console.log("WebSocket 연결 성공.");
     };
+
     // 메시지 수신 시
     this.socket.onmessage = (event) => {
       console.log("서버에서 메시지 수신:", event.data);
-      // 받은 메시지가 숫자인 경우 프로그래스 바 업데이트
-      const progress = parseInt(event.data);
-      if (!isNaN(progress)) {
-        this.progressBarWidth = `${progress}%`;
+
+      // JSON 형식의 데이터를 파싱하여 객체로 변환
+      const data = JSON.parse(event.data);
+      // 진행률, 경과 시간, 남은 시간 추출
+      const { progress, elapsed_time, remain_time } = data;
+      // 진행률 업데이트
+      this.progressBarWidth = `${progress}%`;
+      // 경과 시간을 분과 초로 변환
+      const elapsedMinutes = Math.floor(elapsed_time / 60);
+      const elapsedSeconds = elapsed_time % 60;
+      // 남은 시간을 분과 초로 변환
+      const remainingMinutes = Math.floor(remain_time / 60);
+      const remainingSeconds = remain_time % 60;
+      // 진행 정보 업데이트
+      this.progressInfo = `진행률: ${progress}%, 경과 시간: ${elapsedMinutes
+        .toString()
+        .padStart(2, "0")}분 ${elapsedSeconds.toString().padStart(2, "0")}초, 남은 시간: ${remainingMinutes
+          .toString()
+          .padStart(2, "0")}분 ${remainingSeconds.toString().padStart(2, "0")}초`;
+
+      // 진행률이 100인 경우 소켓 연결 종료
+      if (progress >= 100) {
+        console.log("진행률이 100%입니다. 소켓 연결 종료합니다.");
+        this.socket.close();
       }
     };
+    
     // 연결이 종료되면
     this.socket.onclose = () => {
       console.log("WebSocket 연결 종료.");
@@ -61,7 +86,8 @@ export default {
   width: 300px;
   height: 30px;
   border: 1px solid #ccc;
-  margin: 20px auto; /* 화면 중앙 정렬 */
+  margin: 20px auto;
+  /* 화면 중앙 정렬 */
   position: relative;
 }
 
